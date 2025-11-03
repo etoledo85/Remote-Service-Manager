6488527b2f417f1557b39e58a66801ac7c381dc9
Erick-sysadmin <31049631+etoledo85@users.noreply.github.com>
2025-11-03 11:40:31 -0600

    Add files via upload

---
+++ setservice.ps1
@@ -0,0 +1,16 @@
+﻿$servers = Get-Content $PSScriptRoot\Serverlist.txt
+$Creds = Get-Credential
+
+foreach($server in $servers){
+ 
+$session = New-PSSession -ComputerName $server -Credential $Creds
+
+Invoke-Command -Session $session -Scriptblock {
+
+set-service -Name CDPSvc -StartupType Manual
+
+}
+
+Remove-PSSession $session
+
+}
